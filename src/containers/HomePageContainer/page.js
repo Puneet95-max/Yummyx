84f5bf65c6a1ca5b3de281f5e315019047bb17f0
Simran-1313 <singhsimranpreet916@gmail.com>
2024-03-02 03:45:05 +0530

    Navbar done

--- src/containers/HomePageContainer/page.js
+++ src/containers/HomePageContainer/page.js
@@ -1,11 +1,18 @@
+'use client'
 import Navbar from '@/components/NavbarComponent/Navbar';
+
 import React from 'react'
+import { useState,useEffect } from 'react';
 
 const HomePageContainer = () => {
+
+
+
     return (
         <>
             <main>
-                <Navbar />
+              <Navbar />
+     
             </main>
         </>
     )
