d622a678e7f9ed6c4ef49f050cb396df41f41dfa
Puneet <puneet12345v@example.com>
2024-03-02 05:43:53 -0500

    final nav changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 import Footer from "@/components/FooterComponent/Footer";
-import Navbar from "@/components/NavbarComponent/Navbar";
 import HomePageContainer from "@/containers/HomePageContainer/page";
 
 export default function Home() {
   return (
     <>
-      <section>
-        <Navbar />
-      </section>
       <section>
         <HomePageContainer />
       </section>
