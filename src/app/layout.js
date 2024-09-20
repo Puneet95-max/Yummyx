84b1014f04b34e9aa24dc57e8ebc11456031c81a
Puneet95-max <83499876+Puneet95-max@users.noreply.github.com>
2024-09-20 15:41:27 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,26 @@
-import { Inter, Anta } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-<meta name="google-site-verification" content="rtbTSySVPsmYDfIpbFtS7Vm4Y8xKTuABW30uQxjEqqw" />
 export const metadata = {
   title: "Yummy",
-  description: "Traditional Food resort and  unique recipies",
+  description: "Traditional Food resort and unique recipes",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="rtbTSySVPsmYDfIpbFtS7Vm4Y8xKTuABW30uQxjEqqw" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body className={inter.className}>
         <div>
           {children}
-          <Analytics/>
+          <Analytics />
         </div>
       </body>
     </html>
